c13603d6e3209b3df03c165992411cbbb6e064d1
sanskriti shrestha <137851956+sanskritistha@users.noreply.github.com>
2024-05-30 18:58:10 +0545

    Add files via upload

---
+++ list-sorting.py
@@ -0,0 +1,5 @@
+city=['ktm','Bkt','Pkr','brt']
+city.sort()#case sensetive
+#to reduce case sensentivity
+#city.sort(key=str.lower)
+print(city)
---
+++ listjoin.py
@@ -0,0 +1,18 @@
+#by using + operator
+a=[1,2,3]
+x=['a','b','c']
+z=x+a
+print(z)
+
+#by using append() method:
+a=[1,2,3]
+x=['a','b','c']
+x.append(a)
+print(x)
+
+#by using appemd() method:
+a=[1,2,3]
+x=['a','b','c']
+x.extend(a)
+print(x)
+
