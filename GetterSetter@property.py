4e3337791e049cc4b78149c3280f0e1d9a86a9ee
sanskriti shrestha <137851956+sanskritistha@users.noreply.github.com>
2024-06-28 20:29:04 +0545

    @property decorator

--- GetterSetter@property.py
+++ GetterSetter@property.py
@@ -1,26 +1,21 @@
 #property in python
 class Car:
-    def __init__(self,model,price):
-        self.__model=None
-        self.__price=price
-    @model.setter
-    def  set_model(self,model):
-        self.__model=model
     @property 
-    def get_model(self):
+    def model(self):
         return self.__model
-    @price.setter
-    def set_price(self,price):
-        self.__price=price
+    @model.setter
+    def  model(self,model):
+        self.__model=model
     @property
     def get_price(self):
         return self.__price
-    def show(self):
-        print(self.__model,self.__price)
+    @price.setter
+    def set_price(self,price):
+        self.__price=price
 c=Car()
-c.set_model('nexon')
-c.set_price(500)
-c.model="BMW"
-c.price=2000000
+c.Model='scoripo'
+c.price=150
+print(c.Model,c.Price)
+
 
 print(c.model(),c.price())
